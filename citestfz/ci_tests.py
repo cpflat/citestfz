59afcde0191ed442832d0489a0e619c0af7e4339
sat-mbp <sat@hongo.wide.ad.jp>
2018-03-05 15:02:38 +0900

    remove duplicated code

--- citestfz/ci_tests.py
+++ citestfz/ci_tests.py
@@ -18,12 +18,3 @@ def ci_test_gauss(data_matrix, x, y, s, **kwargs):
     return p_val
 
 
-if __name__ == "__main__":
-    import gauss_testdata
-    dm = np.array(gauss_testdata.data)
-    cm = np.corrcoef(dm.T)
-
-    v = ci_test_gauss(dm, 0, 2, [1], corr_matrix = cm)
-    print("x = 0, y = 2, z = [1] : {0} (R:0.787275)".format(v))
-    v = ci_test_gauss(dm, 0, 2, [], corr_matrix = cm)
-    print("x = 0, y = 2, z = Null : {0} (R:1.11125330537e-53)".format(v))
